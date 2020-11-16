831ae59f47eb3a546b118890551db80445427d45
Artyom Mikhailov <happytoad42@gmail.com>
2020-11-16 10:45:28 +0700

    created graphql schema and plugged it in apollo server

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,38 +1,60 @@
-import { GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } from 'graphql';
+import {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList,
+} from 'graphql';
+import { listings } from './listings';
 
+// Create custom  type fro listings object (just like in fake listings)
 const Listing = new GraphQLObjectType({
   name: 'Listing',
   fields: {
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    image: { type: GraphQLString },
-    address: { type: GraphQLString },
-    price: { type: GraphQLInt },
-    numOfGuests: { type: GraphQLInt },
-    numofBeds: { type: GraphQLInt },
-    numOfBaths: { type: GraphQLInt },
-    rating: { type: GraphQLInt }
-  }
-})
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numofBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLInt) },
+  },
+});
 
+// query request
 const query = new GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => 'Hello from the Query'
-    }
-  }
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => listings,
+    },
+  },
 });
 
+// mutation request e.g. add or delete a listing
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => 'Hello from the Mutation'
-    }
-  }
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id === id) {
+            return listings.splice(i, 1)[0];
+          }
+        }
+      },
+    },
+  },
 });
 
-export const schema = new GraphQLSchema({ query, mutation })
+// create a GraphQL schema with given actions
+export const schema = new GraphQLSchema({ query, mutation });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { schema } from './graphql'
+import { schema } from './graphql';
 
+// create express instance
 const app = express();
 const port = 9000;
 
+// Init Apollo express server with given schema
 const server = new ApolloServer({ schema });
-server.applyMiddleware({ app, path: '/api'})
 
+// use our express instance as server base and declare entry point for API
+server.applyMiddleware({ app, path: '/api' });
 
+// init the app on given port
 app.listen(port);
 
+// for convinience
 console.log(`[app]: http://localhost:${port}`);
