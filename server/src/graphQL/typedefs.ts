1d28ee73fa38e749aba0d790b4fc8c40c0b5c7ee
Artyom Mikhailov <happytoad42@gmail.com>
2021-01-11 17:52:17 +0700

    added new temp resolvers for OAuth

--- server/src/graphQL/typedefs.ts
+++ server/src/graphQL/typedefs.ts
@@ -2,21 +2,11 @@ import { gql } from 'apollo-server-express';
 
 // Type definitions for every schema field there is
 export const typeDefs = gql`
-  type Listing {
-    id: ID!
-    title: String!
-    image: String!
-    address: String!
-    price: Int!
-    numOfGuests: Int!
-    numOfBeds: Int!
-    numOfBaths: Int!
-    rating: Int!
-  }
   type Query {
-    listings: [Listing!]!
+    authUrl: String!
   }
   type Mutation {
-    deleteListing(id: ID!): Listing!
+    logIn: String!
+    logOut: String!
   }
 `;
