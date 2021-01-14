65e4d72e5b53b96a5ed44411fb399b77841d84b7
Artyom Mikhailov <happytoad42@gmail.com>
2021-01-14 13:37:32 +0700

    added login mechanic

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
@@ -13,13 +13,24 @@ import {
   NotFound,
   User,
 } from './sections';
+import { Viewer } from './lib/types';
 import './styles/index.css';
 
 const client = new ApolloClient({
   uri: '/api',
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
   return (
     <Router>
       <Layout id='app'>
@@ -28,7 +39,11 @@ const App = () => {
           <Route exact path='/host' component={Host} />
           <Route exact path='/listing/:id' component={Listing} />
           <Route exact path='/listings/:location?' component={Listings} />
-          <Route exact path='/login' component={Login} />
+          <Route
+            exact
+            path='/login'
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path='/user/:id' component={User} />
           <Route component={NotFound} />
         </Switch>
--- client/src/lib/graphql/index.ts
+++ client/src/lib/graphql/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from './mutations';
+export * from './queries';
